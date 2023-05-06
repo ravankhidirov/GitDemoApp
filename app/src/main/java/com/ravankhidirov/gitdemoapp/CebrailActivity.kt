d2e14c407851b1b0d104e3c7a9913746a5b628b8
Your Name <you@example.com>
2023-05-06 11:28:56 +0400

    i have added new activity

---
+++ app/src/main/java/com/ravankhidirov/gitdemoapp/CebrailActivity.kt
@@ -0,0 +1,11 @@
+package com.ravankhidirov.gitdemoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CebrailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cebrail)
+    }
+}
